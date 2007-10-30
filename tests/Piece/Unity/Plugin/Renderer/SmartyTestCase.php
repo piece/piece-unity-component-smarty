1dc1bc3c1cf23d193400591b5457e90661f36ced
KUBO Atsuhiro <kubo@iteman.jp>
2007-10-30 03:57:44 +0000

    - Added a test case for layered structure support.

--- tests/Piece/Unity/Plugin/Renderer/SmartyTestCase.php
+++ tests/Piece/Unity/Plugin/Renderer/SmartyTestCase.php
@@ -88,8 +88,6 @@ class Piece_Unity_Plugin_Renderer_SmartyTestCase extends Piece_Unity_Plugin_Rend
         $buffer = $this->_render();
 
         $this->assertEquals('Hello World', trim($buffer));
-
-        $this->_clear($viewString);
     }
 
     /**#@-*/
@@ -97,15 +95,6 @@ class Piece_Unity_Plugin_Renderer_SmartyTestCase extends Piece_Unity_Plugin_Rend
     /**#@+
      * @access private
      */
-
-    function _clear($view)
-    {
-        foreach (array('Content', 'Layout', 'Fallback') as $directory) {
-            $smarty = &new Smarty();
-            $smarty->compile_dir = "{$this->_cacheDirectory}/compiled-templates/$directory";
-            $smarty->clear_compiled_tpl("$view.tpl");
-        }
-    }
 
     function &_getConfig()
     {
@@ -122,6 +111,21 @@ class Piece_Unity_Plugin_Renderer_SmartyTestCase extends Piece_Unity_Plugin_Rend
     function _doSetUp()
     {
         $this->_cacheDirectory = dirname(__FILE__) . '/' . basename(__FILE__, '.php');
+    }
+
+    /**
+     * @since Method available since Release 1.1.0
+     */
+    function &_getConfigForLayeredStructure()
+    {
+        $config = &new Piece_Unity_Config();
+        $config->setConfiguration('Dispatcher_Simple', 'actionDirectory', "{$this->_cacheDirectory}/actions");
+        $config->setConfiguration('Renderer_Smarty', 'template_dir', "{$this->_cacheDirectory}/templates");
+        $config->setConfiguration('Renderer_Smarty', 'compile_dir', "{$this->_cacheDirectory}/compiled-templates");
+        $config->setConfiguration('Renderer_Smarty', 'plugins_dir', array("{$this->_cacheDirectory}/plugins"));
+        $config->setExtension('View', 'renderer', 'Renderer_Smarty');
+
+        return $config;
     }
 
     /**#@-*/
